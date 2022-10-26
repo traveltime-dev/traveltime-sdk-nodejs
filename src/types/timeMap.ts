1fe3918e8c9086842cb4fed3bcfcd0c206141f13
EivydasKoc <e.kocinas@gmail.com>
2022-10-26 12:11:56 +0300

    add single_shape and no_holes to time-map

--- src/types/timeMap.ts
+++ src/types/timeMap.ts
@@ -5,118 +5,119 @@ import {
 } from './common';
 
 export type TimeMapRequestProperty = 'is_only_walking';
-
 export declare type TimeMapRequestLevelOfDetailScaleType = 'simple' | 'coarse_grid';
 export declare type TimeMapRequestLevelOfDetailLevel = 'lowest' | 'low' | 'medium' | 'high' | 'highest';
 
 export type RequestLevelOfDetail = {
-  'scale_type': TimeMapRequestLevelOfDetailScaleType;
-  'level': TimeMapRequestLevelOfDetailLevel;
-  'square_size': number;
+  scale_type: TimeMapRequestLevelOfDetailScaleType;
+  level: TimeMapRequestLevelOfDetailLevel;
+  square_size: number;
 }
 
 export type TimeMapRequestSearchBase = {
-  'id': string
-  'coords': Coords
-  'transportation': TransportationRequestCommons;
-  'travel_time': number
-  'properties'?: Array<TimeMapRequestProperty>;
-  'range'?: RangeRequestNoMaxResults
-  'level_of_detail'?: RequestLevelOfDetail
+  id: string
+  coords: Coords
+  transportation: TransportationRequestCommons
+  travel_time: number
+  properties?: Array<TimeMapRequestProperty>
+  range?: RangeRequestNoMaxResults
+  level_of_detail?: RequestLevelOfDetail
+  single_shape?: boolean
+  no_holes?: boolean
 }
 
 export type TimeMapRequestDepartureSearch = TimeMapRequestSearchBase & {
-  'departure_time': string;
+  departure_time: string;
 };
 
 export type TimeMapRequestArrivalSearch = TimeMapRequestSearchBase & {
-  'arrival_time': string
+  arrival_time: string
 };
 
 export type TimeMapRequestUnionOrIntersection = {
-  'id': string;
-  'search_ids': Array<string>;
+  id: string;
+  search_ids: Array<string>;
 }
 
 export type TimeMapRequest = {
-  'departure_searches'?: Array<TimeMapRequestDepartureSearch>
-  'arrival_searches'?: Array<TimeMapRequestArrivalSearch>
-  'unions'?: Array<TimeMapRequestUnionOrIntersection>
-  'intersections'?: Array<TimeMapRequestUnionOrIntersection>
+  departure_searches?: Array<TimeMapRequestDepartureSearch>
+  arrival_searches?: Array<TimeMapRequestArrivalSearch>
+  unions?: Array<TimeMapRequestUnionOrIntersection>
+  intersections?: Array<TimeMapRequestUnionOrIntersection>
 }
 
 export type TimeMapResponseShape = {
-  'shell': Array<Coords>;
-  'holes': Array<Array<Coords>>;
+  shell: Array<Coords>;
+  holes: Array<Array<Coords>>;
 }
 
 export type TimeMapResponseProperties = {
-  'is_only_walking'?: boolean;
+  is_only_walking?: boolean;
 }
 
 export type TimeMapResponseResult = {
-  'search_id': string;
-  'shapes': Array<TimeMapResponseShape>;
-  'properties': TimeMapResponseProperties;
+  search_id: string;
+  shapes: Array<TimeMapResponseShape>;
+  properties: TimeMapResponseProperties;
 }
 
 export type TimeMapResponse = {
-  'results': Array<TimeMapResponseResult>
+  results: Array<TimeMapResponseResult>
 }
 
 export type Position = number[];
 
 export type TimeMapResponseGeoJSONPolygon = {
-  'type': string;
-  'coordinates': Position[][][];
+  type: string;
+  coordinates: Position[][][];
 }
 
 export type TimeMapResponseGeoJSONFeature = {
-  'type': string;
-  'geometry': Array<TimeMapResponseGeoJSONPolygon> | TimeMapResponseGeoJSONPolygon;
-  'properties': TimeMapResponseProperties;
+  type: string;
+  geometry: Array<TimeMapResponseGeoJSONPolygon> | TimeMapResponseGeoJSONPolygon;
+  properties: TimeMapResponseProperties;
 }
 
 export type TimeMapResponseGeoJSON = {
-  'type': string;
-  'features': Array<TimeMapResponseGeoJSONFeature>;
+  type: string;
+  features: Array<TimeMapResponseGeoJSONFeature>;
 }
 
 export type TimeMapResponseVndWktResult = {
-  'search_id': string;
-  'shape': string;
-  'properties': TimeMapResponseProperties;
+  search_id: string;
+  shape: string;
+  properties: TimeMapResponseProperties;
 }
 
 export type TimeMapResponseVndWkt = {
-  'results': Array<TimeMapResponseVndWktResult>;
+  results: Array<TimeMapResponseVndWktResult>;
 }
 
 export type Envelope = {
-  'min_lat': number,
-  'max_lat': number,
-  'min_lng': number,
-  'max_lng': number,
+  min_lat: number,
+  max_lat: number,
+  min_lng: number,
+  max_lng: number,
 }
 
 export type Boxes = {
-  'min_lat': number,
-  'max_lat': number,
-  'min_lng': number,
-  'max_lng': number,
+  min_lat: number,
+  max_lat: number,
+  min_lng: number,
+  max_lng: number,
 }
 
 export type TimeMapResponseVndBoundingBoxesBoundingBox = {
-  'envelope': Envelope;
-  'boxes': Array<Boxes>;
+  envelope: Envelope;
+  boxes: Array<Boxes>;
 }
 
 export type TimeMapResponseVndBoundingBoxesResult = {
-  'search_id': string;
-  'bounding_boxes': Array<TimeMapResponseVndBoundingBoxesBoundingBox>;
-  'properties': TimeMapResponseProperties;
+  search_id: string;
+  bounding_boxes: Array<TimeMapResponseVndBoundingBoxesBoundingBox>;
+  properties: TimeMapResponseProperties;
 }
 
 export type TimeMapResponseVndBoundingBoxes = {
-  'results': Array<TimeMapResponseVndBoundingBoxesResult>;
+  results: Array<TimeMapResponseVndBoundingBoxesResult>;
 }
