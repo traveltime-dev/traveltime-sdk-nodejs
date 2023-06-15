ffbf17afeb5128dd34babbd4e4399fd8e9a645ba
mockusTraveltTime <tomas.mockus@traveltime.com>
2023-06-15 17:32:08 +0300

    remove double mapping

--- src/client/mapper.ts
+++ src/client/mapper.ts
@@ -6,9 +6,7 @@ import {
 } from '../types';
 
 export function timeMapSimpleToRequest(body: TimeMapSimple): TimeMapRequest {
-  const searchBases = body.coords.map<TimeMapRequestSearchBase>((coords, index) => ({
-    id: `id-${index}`,
-    coords,
+  const searchBase : Omit<TimeMapRequestSearchBase, 'id' | 'coords'> = {
     transportation: body.transportation,
     travel_time: body.travelTime,
     level_of_detail: body.level_of_detail,
@@ -16,15 +14,25 @@ export function timeMapSimpleToRequest(body: TimeMapSimple): TimeMapRequest {
     properties: body.properties,
     range: body.range,
     single_shape: body.single_shape,
-  }));
+  };
 
   if (body.searchType === 'arrive') {
     return {
-      arrival_searches: searchBases.map((base) => ({ ...base, arrival_time: body.leaveTime })),
+      arrival_searches: body.coords.map((coords, index) => ({
+        ...searchBase,
+        coords,
+        id: `id-${index}`,
+        arrival_time: body.leaveTime,
+      })),
     };
   }
   return {
-    departure_searches: searchBases.map((base) => ({ ...base, departure_time: body.leaveTime })),
+    departure_searches: body.coords.map((coords, index) => ({
+      ...searchBase,
+      coords,
+      id: `id-${index}`,
+      departure_time: body.leaveTime,
+    })),
   };
 }
 
