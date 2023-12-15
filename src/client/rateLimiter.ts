18d1eb8744cb89af033f704dde90c661549d7d0a
mockusTraveltTime <tomas.mockus@traveltime.com>
2023-12-15 13:57:29 +0200

    reduce memory usage in rate limiter

--- src/client/rateLimiter.ts
+++ src/client/rateLimiter.ts
@@ -1,5 +1,3 @@
-import crypto from 'crypto';
-
 export type RateLimitSettings = {
   enabled: boolean
   hitsPerMinute: number
@@ -9,7 +7,7 @@ type Task<T = any> = () => Promise<T> | T;
 export class RateLimiter {
   private rateLimitSettings: RateLimitSettings;
   private requestQueue: Array<{ task: Task, hits: number }>;
-  private completedQueue: Set<string>;
+  private completedQueueSize: number;
   private isThrottleActive: boolean;
   private isRequestInProgress: boolean;
 
@@ -17,7 +15,7 @@ export class RateLimiter {
     rateLimitSettings?: Partial<RateLimitSettings>,
   ) {
     this.requestQueue = [];
-    this.completedQueue = new Set();
+    this.completedQueueSize = 0;
     this.isThrottleActive = false;
     this.isRequestInProgress = false;
     this.rateLimitSettings = {
@@ -35,11 +33,11 @@ export class RateLimiter {
     }, ((60 * 1000) / this.rateLimitSettings.hitsPerMinute) * hits);
   }
 
-  private taskCleanUp(ids: string[]) {
+  private taskCleanUp(amount: number) {
     this.isRequestInProgress = false;
     if (this.requestQueue.length > 0) this.execute();
     setTimeout(() => {
-      ids.forEach((id) => this.completedQueue.delete(id));
+      this.completedQueueSize -= amount;
       this.execute();
     }, 1000 * 60);
   }
@@ -48,14 +46,13 @@ export class RateLimiter {
     if (this.isRequestInProgress || this.isThrottleActive) return;
     const request = this.requestQueue.shift();
     if (!request) return;
-    if (this.completedQueue.size + request.hits <= this.rateLimitSettings.hitsPerMinute) {
+    if (this.completedQueueSize + request.hits <= this.rateLimitSettings.hitsPerMinute) {
       this.isThrottleActive = true;
       this.isRequestInProgress = true;
-      const uuids = [...Array(request.hits).keys()].map(() => crypto.randomUUID());
-      uuids.forEach((id) => this.completedQueue.add(id));
-      this.disableThrottle(uuids.length);
+      this.completedQueueSize += request.hits;
+      this.disableThrottle(request.hits);
       await request.task();
-      this.taskCleanUp(uuids);
+      this.taskCleanUp(request.hits);
     } else {
       this.requestQueue.unshift(request);
     }
