d41693110ce2ce225d1c26164e7febf871f155ee
mockusTraveltTime <tomas.mockus@traveltime.com>
2024-10-07 13:52:25 +0300

    add more countries

--- src/client/proto/countries.ts
+++ src/client/proto/countries.ts
@@ -1,3 +1,3 @@
 export const protoCountries = [
-  'au', 'at', 'be', 'ca', 'dk', 'fi', 'fr', 'de', 'gr', 'hu', 'in', 'id', 'ie', 'it', 'jp', 'lv', 'lt', 'nl', 'nz', 'no', 'ph', 'pl', 'pt', 'ro', 'sg', 'si', 'za', 'es', 'se', 'ch', 'uk', 'us',
+  'au', 'at', 'be', 'ca', 'dk', 'fi', 'fr', 'de', 'gr', 'hu', 'in', 'id', 'ie', 'it', 'jp', 'lv', 'lt', 'nl', 'nz', 'no', 'ph', 'pl', 'pt', 'ro', 'sg', 'si', 'za', 'es', 'se', 'ch', 'uk', 'us', 'mx', 'rs', 'sa',
 ] as const;
