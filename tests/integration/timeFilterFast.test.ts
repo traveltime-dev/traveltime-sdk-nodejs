4c409861cc46b1a60263c0089ce4d46a20a4fc4a
Arnas Bradauskas <140691866+arnasbr@users.noreply.github.com>
2026-09-01 14:33:35 +0300

    Add walking_time, driving_time_to_station and parking_time to fast transportation (#256)

--- tests/integration/timeFilterFast.test.ts
+++ tests/integration/timeFilterFast.test.ts
@@ -20,6 +20,29 @@ describe('timeFilterFast', () => {
     expect(response.results.length).toBeGreaterThan(0);
   });
 
+  it('should handle driving+public_transport with params', async () => {
+    const response = await client.timeFilterFast({
+      locations,
+      arrival_searches: {
+        one_to_many: [{
+          id: 'driving+pt with params',
+          departure_location_id: 'London center',
+          arrival_location_ids: ['Hyde Park', 'ZSL London Zoo'],
+          arrival_time_period: 'weekday_morning',
+          travel_time: 3600,
+          transportation: {
+            type: 'driving+public_transport',
+            walking_time: 300,
+            driving_time_to_station: 600,
+            parking_time: 120,
+          },
+          properties: ['travel_time'],
+        }],
+      },
+    });
+    expect(response.results.length).toBeGreaterThan(0);
+  });
+
   it('should handle many-to-one search', async () => {
     const response = await client.timeFilterFast({
       locations,
